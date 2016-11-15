21a0af74fece8ff6db6f9a4167a4e4dc2c1deaa6
wubmu <923185028@qq.com>
2016-11-15 17:07:30 +0800

    test

--- Python爬虫/JianshuPC.py
+++ Python爬虫/JianshuPC.py
@@ -6,12 +6,15 @@ page=requests.get(baseUrl).content
 #用beautifusoup解析URL
 soup=BeautifulSoup(page)
 #print(soup.find_all("a",href_=''))
+a=soup.select(".title")
+
+print(a[10].a['href'])
+
 add_url = soup.select(".ladda-button")[-1].get("data-url")
-page2=requests.get(baseUrl+''+add_url)
-soup2=BeautifulSoup(page2)
-#print(soup2.select(".title a"))
-#artict_List=[(i.get_text(),j.get_text) for i,j in (soup.select(".title a"),soup.select("'.title a"))]
-article_list = [i.get_text() for i in soup2.select( '.title')]
+article_list = [i.get_text() for i in soup.select( '.title')]
 for al in  article_list:
     print(al)
-    print(2)
+#artict_List=[(i.get_text(),j.get_text) for i,j in (soup.select(".title a"),soup.select("'.title a"))]
+#article_list = [i.get_text() for i in soup.select( '.title')]
+#for al in  article_list:
+# print(al)
---
+++ Python爬虫/JianshuPC2.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+url=requests.get('http://www.jianshu.com/top/daily?note_ids%5B%5D=6971775&note_ids%5B%5D=6921213&note_ids%5B%5D=6880553&note_ids%5B%5D=6962991&note_ids%5B%5D=6971400&note_ids%5B%5D=6855868&note_ids%5B%5D=6965610&note_ids%5B%5D=6960967&note_ids%5B%5D=6862760&note_ids%5B%5D=6962163&note_ids%5B%5D=6883567&note_ids%5B%5D=6850879&note_ids%5B%5D=6819395&note_ids%5B%5D=6971309&note_ids%5B%5D=6957088&note_ids%5B%5D=6972701&note_ids%5B%5D=6967563&note_ids%5B%5D=6969161&note_ids%5B%5D=6957971&note_ids%5B%5D=6906587&note_ids%5B%5D=6960951&page=2')
+print(url)
+#print(url.content)
+soup=BeautifulSoup(url.content)#content是关键
+i=soup.select('.title a')[10].get('href')
+print(i)
+#article_list = [i.get_text() for i in soup.select( '.title')]
+#for al in  article_list:
+ #   print(al)
